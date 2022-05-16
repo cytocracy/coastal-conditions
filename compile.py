7f3c370b968daa0ed9e85ac19bf42eac15903cdb
cytocracy <theop.parker@gmail.com>
2022-05-15 18:20:52 -0700

    compile working & gitignore for vids

--- compile.py
+++ compile.py
@@ -1,17 +1,30 @@
 import os
-import timeit
-import fnmatch
-import img2video
+import cv2
+from PIL import Image
 
-img_path = './image_out/'
-video_path = './'
-data_path = './'
+os.chdir("D:\\Development\\Chem\\coastal-conditions\\image_out")
+path = "D:\\Development\\Chem\\coastal-conditions\\image_out\\"
 
-def main():
-    img_type = 'uint8_TIFF'
-    video_type = 'mp4'
+mean_height = 0
+mean_width = 0
+num_of_images = len(os.listdir('.'))
 
-    img2video.convert2MP4(video_path, img_type)
+def generate_video():
+    image_folder = "D:\\Development\\Chem\\coastal-conditions\\image_out"
+    video_name = 'video.avi'
+    os.chdir(image_folder)
 
-if __name__ == '__main__':
-    main()
+    images = [img for img in os.listdir(image_folder)
+              if img.endswith(".tiff")]
+
+    print(images)
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter(video_name, 0, 30, (width, height))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+generate_video()
